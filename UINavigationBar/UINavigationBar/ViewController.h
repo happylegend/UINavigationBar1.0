de831e89982ae491f8849fa3939319e44c0b440e
happylegend <jiqiangshenghero@163.com>
2013-09-14 18:00:32 +0800

    none

--- UINavigationBar/UINavigationBar/ViewController.h
+++ UINavigationBar/UINavigationBar/ViewController.h
@@ -14,11 +14,13 @@
  Navigation item表示导航栏的一组条目，是栈的形式组织的，是一个item集合
  BarButtonItem是条目的类型
  
- 导航栏的一般使用方式是，
+ 但是使用导航栏，而不是通过导航控制器来时的时候，的一般使用方式是，
  第一：创建一个Navigation bar
  第二：创建一个Navigation item
  第三：设置Navigation item的各个条目，比如leftbarbuttonitem，title，titleview，backbarbuttonitem，rightbarbuttonitem，backitem，topitem
  第四：将Navigation item显示在Navigation bar上，使用push函数
+ 
+ 单独使用导航栏的时候，要显示条目，要使用navigationbar的push方法，显示出navigationitem条目集合
  */
 
 #import <UIKit/UIKit.h>
